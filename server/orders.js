d112b362df38bcad1992800bee8a3243076e7f40
Michael Chen <michael.chen.cse@gmail.com>
2017-04-18 11:51:45 -0400

    updated users again

--- server/orders.js
+++ server/orders.js
@@ -1,22 +1,11 @@
 'use strict'
 
 const db = require('APP/db')
-const User = db.model('users')
+const User = db.model('orders')
 
 const {mustBeLoggedIn, forbidden, selfOnly} = require('./auth.filters')
 
 module.exports = require('express').Router()
-  .params('id', 
-    (req, res, next, id) => User.findById(id)
-    .then(user => {
-      if(!user) {
-        res.sendStatus(404);
-      } else {
-        req.foundUser = user;
-        next();
-      }
-    })
-    .catch(next))
   .get('/',
     // The forbidden middleware will fail *all* requests to list users.
     // Remove it if you want to allow anyone to list all users on the site.
@@ -24,7 +13,7 @@ module.exports = require('express').Router()
     // If you want to only let admins list all the users, then you'll
     // have to add a role column to the users table to support
     // the concept of admin users.
-    forbidden('listing users is not allowed'),
+    forbidden('listing orders is not allowed'),
     (req, res, next) =>
       User.findAll()
         .then(users => res.json(users))
@@ -34,27 +23,21 @@ module.exports = require('express').Router()
       User.create(req.body)
       .then(user => res.status(201).json(user))
       .catch(next))
-  .get('/:id', mustBeLoggedIn, (req, res, next) => res.json(req.foundUser))
+  .get('/:id',
+    mustBeLoggedIn,
+    (req, res, next) =>
+      User.findById(req.params.id)
+      .then(user => res.json(user))
+      .catch(next))
   .put('/:id',
     selfOnly,
     (req, res, next) => 
-      req.foundUser.updateAttributes(req.body))
-      .spread((count, rows) => res.status(count ? 200 : 400).send())
+      User.findById(req.params.id)
+      .then(user => user.updateAttributes(req.body))
       .catch(next))
   .delete('/:id',
     forbidden('deleting users is not allowed'),
     (req, res, next) => 
-      req.foundUser.destroy()
-      .then(success => res.status(success ? 200 : 400).send())
-      .catch(next))
-  // MIGHT NEED TO CASCADE OUR DELETE
-  .get('/:id/orders', 
-    (req, res, next) => 
-      req.foundUser.getOrders()
-      .then(orders => res.json(orders))
-      .catch(next))
-  .get('/:id/reviews', 
-    (req, res, next) => 
-      req.foundUser.getReviews()
-      .then(reviews => res.json(reviews))
+      User.findById(req.params.id)
+      .then(user => user.destroy())
       .catch(next))
