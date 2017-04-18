a0f0e2c00e620ba49cee554dd74a292a271814d3
Michael Chen <michael.chen.cse@gmail.com>
2017-04-18 11:51:45 -0400

    updated users again

--- server/api.js
+++ server/api.js
@@ -6,6 +6,11 @@ api
   .get('/heartbeat', (req, res) => res.send({ok: true}))
   .use('/auth', require('./auth'))
   .use('/users', require('./users'))
+  .use('/orders', require('./orders'))
+  .use('/orderItems', require('./order_items'))
+  .use('/reviews', require('./reviews'))
+  .use('/products', require('./products'))
+
 
 // No routes matched? 404.
 api.use((req, res) => res.status(404).end())
