2e56da54feed0a3e959330f902e8d4a975bf7750
fourestfire <kewliez@gmail.com>
2017-04-19 14:47:43 -0400

    complete review tests, mostly

--- db/models/review.test.js
+++ db/models/review.test.js
@@ -1,12 +1,12 @@
 'use strict'
 
 const db = require('APP/db')
-    , {Review, User} = db
+    , {Review, User, Product} = db
     , {expect} = require('chai')
 
 /* global describe it before afterEach */
 
-describe('Review', () => {
+describe.only('Review', () => {
   before('Await database sync', () => db.didSync)
   afterEach('Clear the tables', () => db.truncate({ cascade: true }))
 
@@ -19,28 +19,31 @@ describe('Review', () => {
 
     // TEST OUR REVIEWS
     it('creates review if valid', () => {
-      return testUser.createReview({title: "Valid Review", text: "We have at least twenty characters", rating: 3})
+      return testUser.createReview({title: "Valid Review", text: "We have at least twenty characters", rating: 3, productId: 1})
       .then(review => expect(review.id).to.be.a('number'))
     })
 
     it('does not create review if text is blank', () => {
-      return testUser.createReview({title: "No Text; Should Fail", rating: 1})
+      return testUser.createReview({title: "No Text; Should Fail", rating: 1, productId: 1})
       .catch(err => expect(err.message).to.eql('notNull Violation: text cannot be null'))
     })
 
     it('does not create review if text is less than 20 characters', () => {
-      return testUser.createReview({title: "Invalid Review", text: "Too few characters", rating: 2})
+      return testUser.createReview({title: "Invalid Review", text: "Too few characters", rating: 2, productId: 1})
       .catch(err => expect(err.message).to.eql('Validation error: Review must have at least 20 characters'))
     })
 
     it('does not create review if rating is blank', () => {
-      return testUser.createReview({title: "Invalid Review", text: "This is an invalid rating because the rating is null"})
+      return testUser.createReview({title: "Invalid Review", text: "This is an invalid rating because the rating is null", productId: 1})
       .catch(err => expect(err.message).to.eql('notNull Violation: rating cannot be null'))
     })
 
-    it('does not create review if rating is not within 1 to 5', () => {
-      return testUser.createReview({title: "Invalid Review", text: "This is an invalid rating because the rating is above 7", rating: 7})
-      .catch(err => expect(err.message).to.eql('Validation error: Validation max failed'))
+    // THIS TEST SHOULD NOT BE PASSING: Due to lack of product_id foreign key
+    it('does not create review if there is no product attached', () => {
+      return testUser.createReview({title: "Invalid Review", text: "This review has no product_id foreignKey", rating: 4})
+      .then(thing => console.log(thing))
+      .catch(err => expect(err.message).to.eql('Validation error: Valid'))
     })
+
   })
 })
