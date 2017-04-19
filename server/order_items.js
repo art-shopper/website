9d276fafe706cd529e0ce8246acc470b3e980bb2
fourestfire <kewliez@gmail.com>
2017-04-19 12:08:10 -0400

    finished basic review tests

--- server/order_items.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-
-const db = require('APP/db')
-const Order = db.model('orders')
-
-const {mustBeLoggedIn, forbidden, selfOnly} = require('./auth.filters')
-
-module.exports = require('express').Router()
-
