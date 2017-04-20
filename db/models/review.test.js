cb028cf7abbc3cdc12460c59f864e48c58db6ccb
Edward Goo <Edwardx89@yahoo.com>
2017-04-20 12:41:51 -0400

    fixed false positive test specs

--- db/models/review.test.js
+++ db/models/review.test.js
@@ -28,23 +28,26 @@ describe.only('Review', () => {
 
     it('does not create review if text is blank', () => {
       return testUser.createReview({title: 'No Text; Should Fail', rating: 1, product_id: 1})
+      .then((data) => expect(data).to.eql("This is a false positive. You should be reaching the .catch"))
       .catch(err => expect(err.message).to.eql('notNull Violation: text cannot be null'));
     });
 
     it('does not create review if text is less than 20 characters', () => {
       return testUser.createReview({title: 'Invalid Review', text: 'Too few characters', rating: 2, product_id: 1})
+      .then((data) => expect(data).to.eql("This is a false positive. You should be reaching the .catch"))
       .catch(err => expect(err.message).to.eql('Validation error: Review must have at least 20 characters'));
     });
 
     it('does not create review if rating is blank', () => {
       return testUser.createReview({title: 'Invalid Review', text: 'This is an invalid rating because the rating is null', product_id: 1})
+      .then((data) => expect(data).to.eql("This is a false positive. You should be reaching the .catch"))
       .catch(err => expect(err.message).to.eql('notNull Violation: rating cannot be null'));
     });
 
     // THIS TEST SHOULD NOT BE PASSING: Due to lack of product_id foreign key
     it('does not create review if there is no product attached', () => {
       return testUser.createReview({title: 'Invalid Review', text: 'This review has no product_id foreignKey', rating: 4})
-      .then((res) => console.log('LOGGGINGNG', res))
+      .then((data) => expect(data).to.eql("This is a false positive. You should be reaching the .catch"))
       .catch(err => {
         expect(err.message).to.eql('notNull Violation: product_id cannot be null')
       });
