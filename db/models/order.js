275c6a45e6f370ecebf27b9a9279e86ca80decea
Edward Goo <Edwardx89@yahoo.com>
2017-04-20 11:06:31 -0400

    updated models for order.  Added status to the order model and add some validators

--- db/models/order.js
+++ db/models/order.js
@@ -3,8 +3,21 @@
 const {DATE, STRING} = require('sequelize');
 
 module.exports = db => db.define('orders', {
-  date_fulfilled: DATE,
-  email: STRING
+  date_fulfilled: {
+    type: DATE,
+    defaultValue: Date.now()
+  },
+  email: {
+    type: STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    }
+  },
+  status: {
+    type: STRING,
+    defaultValue: "Pending"
+  },
 });
 
 module.exports.associations = (Order, {User, OrderItem}) => {
