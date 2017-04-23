541db2ec4d6bf64980c882fd02ca72487493ce29
Michael Chen <michael.chen.cse@gmail.com>
2017-04-22 20:31:43 -0400

    updated products reducer

--- app/reducers/products.jsx
+++ app/reducers/products.jsx
@@ -1,3 +1,9 @@
+/*****
+  Products redux set will deal with single and multiple products.
+  Single review in state.products.selected.
+  Multiple reviews in state.products.list.
+*****/
+
 /* -------------------<   ACTIONS   >--------------------- */
 
 const SET_PRODUCT_LIST = 'SET_PRODUCT_LIST';
@@ -9,6 +15,10 @@ export const setProducts = products => ({
   type: SET_PRODUCT_LIST, products
 })
 
+export const setProduct = product => ({
+  type: SET_CURRENT_PRODUCT, product
+})
+
 /* -------------------<   REDUCERS   >--------------------- */
 
 const initialState = {
@@ -18,6 +28,10 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case SET_PRODUCT_LIST:
+      return Object.assign({}, state, {list: action.products});
+    case SET_CURRENT_PRODUCT:
+      return Object.assign({}, state, {selected: action.product})
   }
   return state;
 };
@@ -26,13 +40,17 @@ const reducer = (state = initialState, action) => {
 import axios from 'axios';
 
 // Fetch all products (with search)
-export const fetchProducts = dispatch => {
+export const fetchProducts = () => dispatch => {
   axios.get('/api/products')
     .then(products => dispatch(setProducts(products)))
     .catch(err => console.log(err)) // TODO: real err handling
 };
 
 // Fetch a single product
-
+export const fetchProducts = (productId) => dispatch => {
+  axios.get(`/api/products/${productId}`)
+    .then(product => dispatch(setProduct(product)))
+    .catch(err => console.log(err)) // TODO: real err handling
+};
 
 export default reducer;
