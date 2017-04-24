71eb907b258427d95459feaf89d83a6abe2152af
Edward Goo <Edwardx89@yahoo.com>
2017-04-24 14:45:19 -0400

    add action creators and dispatchers for orders

--- app/reducers/orders.jsx
+++ app/reducers/orders.jsx
@@ -6,10 +6,17 @@ const ADD_ORDER = 'ADD_ORDER';
 
 /* ---------------<   ACTION CREATORS   >------------------- */
 
-// export const authenticated = user => ({
-//   type: AUTHENTICATED, user
-// })
+export const addOrder = order => ({
+  type: ADD_ORDER, order
+})
 
+export const currentOrder = order => ({
+  type: SET_CURRENT_ORDER, order
+})
+
+export const allOrders = orders => ({
+  type: SET_ORDERS_LIST, orders
+})
 /* -------------------<   REDUCERS   >--------------------- */
 
 const initialState = {
@@ -27,9 +34,27 @@ const reducer = (state = initialState, action) => {
 import axios from 'axios';
 
 // Fetch all orders (for the current user)
-
+export const getUserOrder = (userId) => dispatch => {
+  axios.get(`/api/users/{userId}/orders`)
+  .then(res => res.data)
+  .then(orders => dispatch(allOrders(orders)))
+  .catch(err => console.log(err))
+};
 
 // Fetch a single order
+export const getCurrentOrder = (orderId) => dispatch => {
+  axios.get(`/api/orders/{orderId}`)
+  .then(res => res.data)
+  .then(order => dispatch(currentOrder(order)))
+  .catch(err => console.log(err))
+};
 
+// Fetch all orders
+export const getAllOrders = () => dispatch => {
+  axios.get(`/api/orders`)
+  .then(res => res.data)
+  .then(orders => dispatch(allOrders(orders)))
+  .catch(err => console.log(err))
+};
 
 export default reducer;
