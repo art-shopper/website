c76ebeec82b730ec844de5eecd921cfe44b7b6af
Edward Goo <Edwardx89@yahoo.com>
2017-04-24 13:57:51 -0400

    added reducers for cart.jsx

--- app/reducers/cart.jsx
+++ app/reducers/cart.jsx
@@ -1,14 +1,22 @@
 /* -------------------<   ACTIONS   >--------------------- */
 
-const SET_CART_LIST = 'SET_CART_LIST';
 const ADD_ORDER_ITEM ='ADD_ORDER_ITEM';
+const SUBMIT_ORDER = 'SUBMIT_ORDER';
+const DELETE_ITEM = 'DELETE_ITEM';
 
 /* ---------------<   ACTION CREATORS   >------------------- */
 
-// export const authenticated = user => ({
-//   type: AUTHENTICATED, user
-// })
+export const addProduct = product => ({
+  type: ADD_ORDER_ITEM, product
+})
 
+export const deleteItem = product => ({
+  type: DELETE_ITEM, product
+})
+
+export const submitOrder = order => ({
+  type: SUBMIT_ORDER, order
+})
 /* -------------------<   REDUCERS   >--------------------- */
 
 const initialState = {
@@ -17,6 +25,20 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
+    case ADD_ORDER_ITEM:
+      return Object.assign({}, state, {
+        list: [action.product, ... state.list]
+      })
+    case DELETE_ITEM:
+      return {
+        list: state.list.filter(function(elm) {
+          return elm.product_id !== action.product.product_id
+        })
+      }
+    case submitOrder:
+      return {
+        list: []
+      }
   }
   return state
 };
