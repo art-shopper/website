5cde275d18f67802b4613722ed8dcd1439f42e1c
Jason <jason.miguel@gmail.com>
2017-04-25 16:09:07 -0400

    between test

---
+++ tests/reducers/cart.test.js
@@ -0,0 +1,36 @@
+// import React from 'react';
+// import chai, { expect } from 'chai';
+// chai.use(require('chai-enzyme')());
+// import { shallow } from 'enzyme';
+// import { spy } from 'sinon';
+// chai.use(require('sinon-chai'));
+// import { createStore } from 'redux';
+//
+// import reducer, {addProduct, deleteItem, clearCart} from '../../app/reducers/cart';
+//
+// describe.only("Cart", () => {
+//   const state = {
+//     list: { name: 'Dr. Bones',
+//     user_id: 1,
+//     title: 'a painting',
+//     price: '1000',
+//     quantity: 1 }
+//
+//   };
+//
+//   let root, store, dispatch;
+//   beforeEach('create store and render the root', () => {
+//     store = createStore(state => state, state);
+//     dispatch = spy(store, 'dispatch');
+//     root = shallow(<reducer store={store} />);
+//   });
+//
+//   describe("reducer", () => {
+//     it('should return the initial state', () => {
+//       console.log(store.getState());
+//       expect(store.getState()).to.be.deep.equal(state);
+//     });
+//   });
+//
+//   it('should add')
+});
---
+++ tests/reducers/reviews.test.js
@@ -0,0 +1,32 @@
+// import React from 'react';
+// import chai, { expect } from 'chai';
+// chai.use(require('chai-enzyme')());
+// import { shallow } from 'enzyme';
+// import { spy } from 'sinon';
+// chai.use(require('sinon-chai'));
+// import { createStore } from 'redux';
+//
+// import reducer, {addProduct, deleteItem, clearCart} from '../../app/reducers/cart';
+//
+// describe.only("Cart", () => {
+//   const state = {
+//     list: { name: 'Dr. Bones',
+//     title: 'a painting',
+//     price: '1000',
+//     quantity: '1' }
+//
+//   };
+//
+//   let root, store, dispatch;
+//   beforeEach('create store and render the root', () => {
+//     store = createStore(state => state, state);
+//     dispatch = spy(store, 'dispatch');
+//     root = shallow(<reducer store={store} />);
+//   });
+//
+//   describe("reducer", () => {
+//     it('should return the initial state', () => {
+//       expect(reducer(undefined, {})).to.equal(state);
+//     });
+//   });
+// });
