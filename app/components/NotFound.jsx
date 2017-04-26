1b011d962bca9f97d9cb77746aeeaff949c843ce
Michael Chen <michael.chen.cse@gmail.com>
2017-04-26 00:06:16 -0400

    feat glorious bob ross 404 page

--- app/components/NotFound.jsx
+++ app/components/NotFound.jsx
@@ -5,15 +5,7 @@ const NotFound = props => {
   const {pathname} = props.location || {pathname: '<< no path >>'}
   console.error('NotFound: %s not found (%o)', pathname, props)
   return (
-    <div>
-      <h1>Sorry, I couldn't find <pre>{pathname}</pre></h1>
-      <p>The router gave me these props:</p>
-      <pre>
-        {JSON.stringify(props, null, 2)}
-      </pre>
-      <p>Lost? <Link to="/">Here's a way home.</Link></p>
-      <cite>~ xoxo, bones.</cite>
-    </div>
+      <Link to="/"><img src="http://i.imgur.com/DCDflSs.jpg" /></Link>
   )
 }
 
