76a03705129e52b43ed55a2abb45dc31b78d1b95
fourestfire <kewliez@gmail.com>
2017-06-26 15:04:18 -0400

    refactor: move minimzation from package.json to webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -42,7 +42,7 @@ module.exports = {
     ? [new LiveReloadPlugin({appendScriptTag: true})]
     : [new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify('production')
+        NODE_ENV: JSON.stringify('production'),
       }
-    })]
-}
+    }), new webpack.optimize.UglifyJsPlugin()]
+};
